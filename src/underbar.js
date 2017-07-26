eaced09cc01e48f375ea2e772b26238d0ef43861
Dominic Ma <dominic.c.ma@gmail.com>
2017-07-25 23:16:38 -0700

    "Finished _.uniq function"

--- src/underbar.js
+++ src/underbar.js
@@ -117,6 +117,15 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var result = [];
+
+    _.each(array, function(element) {
+      if(!result.includes(element)) {
+        result.push(element);
+      }
+    });
+    
+    return result;
   };
 
 
