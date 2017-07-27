0f12cad8f6fdab3784efc4b6d6e2382bd8a2b6aa
Dominic Ma <dominic.c.ma@gmail.com>
2017-07-26 17:27:32 -0700

    "Finished _.reduce but there is a bug with contain"

--- src/underbar.js
+++ src/underbar.js
@@ -134,6 +134,13 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
+
+    for(var i = 0; i < collection.length; i++) {
+      result.push(iterator(collection[i], i, collection));
+    }
+
+    return result;
   };
 
   /*
@@ -175,6 +182,26 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+
+    // _.reduce takes a collection. Does the iterator function on each element. Returns accumulator.
+    
+    var memo = accumulator;
+
+    for(var i = 0; i < collection.length; i++) {
+      if(memo === undefined) {
+        memo = collection[0];
+        i++;
+      } 
+
+      var item = collection[i];
+
+      var newMemo = iterator(memo, item);
+
+      if(!(newMemo === undefined)) {
+        memo = newMemo;
+      } 
+    }
+    return memo;
   };
 
   // Determine if the array or object contains a given value (using `===`).
