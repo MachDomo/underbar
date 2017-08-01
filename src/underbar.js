ed0bd9f42c002ff5b189e5f98bafc448a0c8ebce
Dominic Ma <dominic.c.ma@gmail.com>
2017-07-31 23:37:55 -0700

    "Worked on memoize"

--- src/underbar.js
+++ src/underbar.js
@@ -359,6 +359,17 @@
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+
+    
+    var result = {};
+
+    return function() {
+      if (!result.hasOwnProperty(arguments)) {
+        result[arguments] = func.apply(this, arguments);
+        
+      } 
+        return result[arguments];
+    }
   };
 
   // Delays a function for the given number of milliseconds, and then calls
